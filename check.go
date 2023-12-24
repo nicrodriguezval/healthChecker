867f5a57f4c2050175876b436ce69c16d882e580
nicrodriguezval <nicrodriguezval@gmail.com>
2023-12-23 19:43:39 -0500

    Some changes to check.go

--- check.go
+++ check.go
@@ -12,8 +12,8 @@ func check(destination, port string) string {
 
 	conn, err := net.DialTimeout("tcp", address, timeout)
 	if err != nil {
-		return fmt.Sprintf("[DOWN] %s is unreachable: %v", address, err)
+		return fmt.Sprintf("[DOWN] %s is unreachable: %v\n", address, err)
 	}
 
-	return fmt.Sprintf("[UP] %s is is reachable, from %v to %v", address, conn.LocalAddr(), conn.RemoteAddr())
+	return fmt.Sprintf("[UP] %s is is reachable, from %v to %v\n", address, conn.LocalAddr(), conn.RemoteAddr())
 }
